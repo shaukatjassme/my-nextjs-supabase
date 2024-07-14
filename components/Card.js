fcd92f7250d1796a3ff3517c4c1baa932e69ae7d
Shaukatjassme <shaukat.jassme786@gmail.com>
2024-07-15 00:53:57 +0500

    card-updated

--- components/Card.js
+++ components/Card.js
@@ -1,33 +1,28 @@
-import React, { useState } from "react";
-import { useRouter } from "next/router";
-import supabase from "../utils/supabase";
+import React, { useState, useEffect } from "react";
+// import Link from "next/router";
+import Link from "next/link";
 
-const Card = () => {
+const Card = ({ onSubmit, editItem }) => {
   const [title, setTitle] = useState("");
   const [address, setAddress] = useState("");
-  const [phone , setPhone] = useState("");
+  const [phone, setPhone] = useState("");
 
-  const router = useRouter();
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-
-    const { data, error } = await supabase
-      .from("transfer")
-      .insert({ title: title, address: address, phone: phone });
-
-    if (error) {
-      console.error("Error inserting data:", error);
+  useEffect(() => {
+    if (editItem) {
+      setTitle(editItem.title);
+      setAddress(editItem.address);
+      setPhone(editItem.phone);
     } else {
-      console.log("Data inserted successfully:", data);
       setTitle("");
       setAddress("");
-       // Redirect to the table page
+      setPhone("");
     }
-  }
+  }, [editItem]);
 
-  const handleRedirect = () => {
-    router.push("/table");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = { title, address, phone };
+    onSubmit(data);
   };
 
   const handleInputChange = (e) => {
@@ -36,19 +31,23 @@ const Card = () => {
       setTitle(value);
     } else if (name === "address") {
       setAddress(value);
-    }
-    else if (name === "phone") {
+    } else if (name === "phone") {
       setPhone(value);
     }
   };
 
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center ">
       <div className="w-96 bg-white p-6 rounded-lg shadow-md">
-        <h1 className="text-2xl font-bold mb-4">Submit Transfer Details</h1>
+        <h1 className="text-2xl font-bold mb-4">
+          {editItem ? "Edit" : "Submit"} Transfer Details
+        </h1>
         <form onSubmit={handleSubmit} className="space-y-4">
           <div>
-            <label htmlFor="title" className="block text-sm font-medium text-gray-700">
+            <label
+              htmlFor="title"
+              className="block text-sm font-medium text-gray-700"
+            >
               Title
             </label>
             <input
@@ -62,7 +61,10 @@ const Card = () => {
             />
           </div>
           <div>
-            <label htmlFor="address" className="block text-sm font-medium text-gray-700">
+            <label
+              htmlFor="address"
+              className="block text-sm font-medium text-gray-700"
+            >
               Address
             </label>
             <input
@@ -74,10 +76,12 @@ const Card = () => {
               placeholder="Enter Address"
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             />
-
           </div>
           <div>
-            <label htmlFor="phone" className="block text-sm font-medium text-gray-700">
+            <label
+              htmlFor="phone"
+              className="block text-sm font-medium text-gray-700"
+            >
               Phone
             </label>
             <input
@@ -94,15 +98,15 @@ const Card = () => {
             type="submit"
             className="w-full px-4 py-2 bg-indigo-600 text-white rounded-md shadow-md hover:bg-indigo-700 focus:outline-none focus:bg-indigo-700"
           >
-            Submit
+            {editItem ? "Update" : "Submit"}
           </button>
+
+          <Link href="/table">
+            <button className="mt-2 block w-full px-4 py-2 bg-green-600 text-white rounded-md shadow-md hover:bg-green-700 focus:outline-none focus:bg-red-700">
+              See Table Data
+            </button>
+          </Link>
         </form>
-        <button
-          onClick={handleRedirect}
-          className="mt-4 w-full px-4 py-2 bg-green-600 text-white rounded-md shadow-md hover:bg-green-700 focus:outline-none focus:bg-green-700"
-        >
-          See Table
-        </button>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from "react";
 import Card from "../components/Card";
 import supabase from "../utils/supabase";
 
-const Home = ({ customProp }) => {
-  const [transferData, setTransferData] = useState(null);
+const Home = () => {
+  const [transferData, setTransferData] = useState([]);
   const [showAlert, setShowAlert] = useState(false);
+  const [editItem, setEditItem] = useState(null);
 
   useEffect(() => {
     fetchData();
@@ -12,7 +13,7 @@ const Home = ({ customProp }) => {
 
   async function fetchData() {
     try {
-      const { data, error } = await supabase.from("transfer").select('*');
+      const { data, error } = await supabase.from("transfer").select("*");
       if (error) {
         throw error;
       }
@@ -23,21 +24,42 @@ const Home = ({ customProp }) => {
   }
 
   async function handleSubmit(data) {
+    if (editItem) {
+      await handleUpdate(data);
+    } else {
+      await handleCreate(data);
+    }
+  }
+
+  async function handleCreate(data) {
     try {
       const { error } = await supabase.from("transfer").insert(data);
       if (error) {
         throw error;
       }
-      setTransferData([...transferData, data]); // Assuming you're adding new data to the list
-      setShowAlert(true); // Show the alert
-      // Fetch updated data after submission
       fetchData();
-      // Hide alert after 3 seconds
-      setTimeout(() => {
-        setShowAlert(false);
-      }, 3000);
+      setShowAlert(true);
+      setTimeout(() => setShowAlert(false), 3000);
     } catch (error) {
       console.error("Error inserting data:", error);
+    }
+  }
+
+  async function handleUpdate(data) {
+    try {
+      const { error } = await supabase
+        .from("transfer")
+        .update(data)
+        .eq("id", editItem.id);
+      if (error) {
+        throw error;
+      }
+      await fetchData(); // Fetch updated data after update
+      setShowAlert(true);
+      setEditItem(null);
+      setTimeout(() => setShowAlert(false), 3000);
+    } catch (error) {
+      console.error("Error updating data:", error);
     }
   }
 
@@ -47,24 +69,87 @@ const Home = ({ customProp }) => {
       if (error) {
         throw error;
       }
-      // Remove the deleted item from state
-      setTransferData(prevData => prevData.filter(item => item.id !== id));
-      console.log(`Deleted item with ID ${id}`);
+      await fetchData(); // Fetch updated data after delete
     } catch (error) {
-      console.error("Error deleting item:", error.message);
+      console.error("Error deleting item:", error);
     }
   }
 
+  const handleEdit = (item) => {
+    setEditItem(item);
+  };
+
   return (
-    <>
-      {showAlert && (
-        <div className="bg-green-200 border-l-4 border-green-500 text-green-700 p-4 mb-4" role="alert">
-          <p className="font-bold">Success!</p>
-          <p>Data submitted successfully.</p>
-        </div>
-      )}
-      <Card customProp={customProp} onSubmit={handleSubmit} />
-    </>
+    <div className="container mx-auto">
+     <div className="grid grid-cols-1 md:grid-cols-12 gap-10">
+  <div className="last-div-component md:col-span-4 mt-8">
+    {showAlert && (
+      <div
+        className="bg-green-200 border-l-4 border-green-500 text-green-700 p-4 mb-4"
+        role="alert"
+      >
+        <p className="font-bold">Success!</p>
+        <p>Data submitted successfully.</p>
+      </div>
+    )}
+
+    <Card onSubmit={handleSubmit} editItem={editItem} />
+  </div>
+  
+  <div className="first-div-component md:col-span-8 mt-8">
+    <h2 className="text-2xl font-bold mb-4">Transfer Data</h2>
+
+    <div className="overflow-x-auto">
+      <table className="min-w-full bg-white border border-gray-200 divide-y divide-gray-200">
+        <thead className="bg-gray-50">
+          <tr>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              ID
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Title
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Address
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Phone
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Action
+            </th>
+          </tr>
+        </thead>
+        <tbody className="divide-y divide-gray-200">
+          {transferData.map((item) => (
+            <tr key={item.id}>
+              <td className="px-6 py-4 whitespace-nowrap">{item.id}</td>
+              <td className="px-6 py-4 whitespace-nowrap">{item.title}</td>
+              <td className="px-6 py-4 whitespace-nowrap">{item.address}</td>
+              <td className="px-6 py-4 whitespace-nowrap">{item.phone}</td>
+              <td className="px-6 py-4 whitespace-nowrap">
+                <button
+                  onClick={() => handleEdit(item)}
+                  className="mr-2 text-sm bg-blue-500 hover:bg-blue-600 text-white py-1 px-3 rounded focus:outline-none focus:bg-blue-600"
+                >
+                  Update
+                </button>
+                <button
+                  onClick={() => handleDelete(item.id)}
+                  className="mr-2 text-sm bg-red-500 hover:bg-red-600 text-white py-1 px-3 rounded focus:outline-none focus:bg-red-600"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+
+    </div>
   );
 };
 
